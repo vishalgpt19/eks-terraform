ad3750ad5186cc9dc449e59a049983ae0584eaff
Vishal Gupta <vishal.gupta@hashedin.com>
2019-06-25 11:56:58 +0000

    Region Changes

--- modules/eks/providers.tf
+++ modules/eks/providers.tf
@@ -1,13 +1,11 @@
 #
+
 # Not required: currently used in conjuction with using
 # icanhazip.com to determine local workstation external IP
 # to open EC2 Security Group access to the Kubernetes cluster.
 # See workstation-ip.tf for additional information.
 #
 
-provider "aws" {
-  region = "${var.aws-region}"
-}
 
 provider "http" {}
 
